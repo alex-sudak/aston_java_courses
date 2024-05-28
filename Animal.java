38b13bf27eaa096640002d7eb45605ad9ce6a44d
alex-sudak <al13khattab@gmail.com>
2024-05-28 23:32:08 +0300

    task1 was end partually 50%

--- Animal.java
+++ Animal.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Animal {
     protected int distance;
-    protected int count;
+    static int count;
     protected String name;
     protected int swim;
     protected int run;
@@ -9,72 +11,100 @@ public class Animal {
         count++;
     }
 
-    void Run(){
+    void Run(int distance){
         System.out.println("Животное бежит " + distance);
     }
-    void Swim(){
+    void Swim(int distance){
         System.out.println("Животное плывет " + distance);
     }
-}
-
-public class Cat extends Animal{
-    int count = 0;
-    public Cat(String name, int swim, int run){
-        this.name = name;
-        this.run = run;
-        this.swim = swim;
-    }
-    @Override
-    void Run(int distance){
-    if(distance > 200){
-        System.out.println("Животное типа 'Кот'" + name + " не может пробегать такое расстояние " + distance);
-    } else {
-        System.out.println("Животное типа 'Кот' " + name + " пробежало " + distance + " метров");
-    }
-}
-    @Override
-    void Swim(int distance) {
-        System.out.println("Животное типа 'Кот'" + name + " не умеет плавать");
-    }
-
-    public int getCount(){
+    public static int getCount(){
         return count;
     }
 }
 
-public class Dog extends Animal{
-    int count = 0;
-    public Dog(String name, int swim, int run){
+class Cat extends Animal{
+    String name;
+    static int count = 0;
+    public Cat(String name){
+        super();
+        count++;
         this.name = name;
-        this.run = run;
-        this.swim = swim;
+    }
+
+    @Override
+    void Run(int distance){
+    if(distance > 200){
+        System.out.println("Животное типа 'Кот' " + name + " тяжело пробегать такое расстояние как " + distance + " метров");
+    } else {
+        System.out.println("Животное типа 'Кот' " + name + " с легкостью пробежал(а) " + distance + " метров");
+    }
+}
+    @Override
+    void Swim(int distance) {
+        System.out.println("Животное типа 'Кот' " + name + " вообще-то не умеет плавать");
+    }
+
+    public static int getCount(){
+        return count;
+    }
+}
+
+class Dog extends Animal{
+    String name;
+    static int count = 0;
+    public Dog(String name){
+        super();
+        count++;
+        this.name = name;
     }
     @Override
     void Run(int distance){
         if(distance > 500){
-            System.out.println("Животное типа 'Собака'" + name + " не может пробегать такое расстояние " + distance);
+            System.out.println("Животное типа 'Собака' " + name + " не может пробегать такое большое расстояние в " + distance + " метр(ов)");
         } else {
-            System.out.println("Животное типа 'Собака' " + name + " пробежало " + distance + " метров");
+            System.out.println("Животное типа 'Собака' " + name + " справил(ся/ась) с дистанцией и пробежал(а) " + distance + " метр(ов)");
         }
     }
     @Override
     void Swim(int distance){
         if(distance > 10){
-            System.out.println("Животное типа 'Собака'" + name + " не может плавать такое расстояние " + distance);
+            System.out.println("Животное типа 'Собака' " + name + " не может плавать такое расстояние как " + distance + " метр(ов)");
         } else {
-            System.out.println("Животное типа 'Собака' " + name + " проплыло " + distance + " метров");
+            System.out.println("Животное типа 'Собака' " + name + " успешно проплыла " + distance + " метр(ов)");
         }
     }
 
-    public int getCount(){
+    public static int getCount(){
         return count;
     }
 }
 
-public Class Main{
+class Main {
     public static void main(String[] args){
         Animal animal = new Animal();
+        System.out.println("Укажите имя КОТА, которое Вам нравится");
+        Scanner cs = new Scanner(System.in);
+        String catname = cs.nextLine();
+        System.out.println("Укажите имя СОБАКИ, которое Вам нравится");
+        String dogname = cs.nextLine();
+        Cat name1 = new Cat(catname);
+        Dog name2 = new Dog(dogname);
+        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОБЕЖАЛ КОТ по имени " +catname);
+        int a = cs.nextInt();
+        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОБЕЖАЛА СОБАКА по имени " +dogname);
+        int b = cs.nextInt();
+        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОПЛЫЛ КОТ по имени " +catname);
+        int c = cs.nextInt();
+        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОПЛЫЛА СОБАКА по имени " +dogname);
+        int d = cs.nextInt();
+        name1.Run(a);
+        name2.Run(b);
+        name1.Swim(c);
+        name2.Swim(d);
+        System.out.println("Всего животных было использовано в эксперименте " + Animal.getCount());
+        System.out.println("Всего котов понадобилось " + Cat.getCount());
+        System.out.println("Всего собак понадобилось " + Dog.getCount());
 
     }
-        }
+}
 
