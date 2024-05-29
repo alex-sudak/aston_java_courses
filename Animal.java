159f0bc2fc8c1b6eaf73ef07223d27e69103c51d
alex-sudak <al13khattab@gmail.com>
2024-05-29 14:26:51 +0300

    task1 was done fully + part of task2(need to change and edit for resolve some issues inside classes

--- Animal.java
+++ Animal.java
@@ -79,8 +79,47 @@ class Dog extends Animal{
     }
 }
 
+public class Bowl{
+    public int hungry;
+    public boolean full;
+    public String name;
+    int add;
+    int del;
+    public int feed;
+    public Bowl(int feed){
+        this.feed = feed;
+    }
+    public void addfeed(int add){
+        feed = feed + add;
+    }
+    public void delfeed(int del){
+        feed = feed - del;
+    }
+    public void total(){
+        System.out.println("Еды в миске осталось: " + feed);
+    }
+}
+
+public class Catfeed extends Bowl{
+//    private int hungry;
+//    public boolean full;
+ //   public String name;
+
+    public Catfeed(String name, int hungry, boolean full){
+        super();
+        this.name = name;
+        this.hungry=  hungry;
+        this.full = full;
+    }
+
+    public void feedAdd(Bowl bwl){
+        bwl.addfeed(hungry);
+    }
+}
+
 class Main {
-    public static void main(String[] args){
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
         Animal animal = new Animal();
         System.out.println("Укажите имя КОТА, которое Вам нравится");
         Scanner cs = new Scanner(System.in);
@@ -89,13 +128,13 @@ class Main {
         String dogname = cs.nextLine();
         Cat name1 = new Cat(catname);
         Dog name2 = new Dog(dogname);
-        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОБЕЖАЛ КОТ по имени " +catname);
+        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОБЕЖАЛ КОТ по имени " + catname);
         int a = cs.nextInt();
-        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОБЕЖАЛА СОБАКА по имени " +dogname);
+        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОБЕЖАЛА СОБАКА по имени " + dogname);
         int b = cs.nextInt();
-        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОПЛЫЛ КОТ по имени " +catname);
+        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОПЛЫЛ КОТ по имени " + catname);
         int c = cs.nextInt();
-        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОПЛЫЛА СОБАКА по имени " +dogname);
+        System.out.println("Укажите дистанцию в метрах, которую Вы бы хотели чтобы ПРОПЛЫЛА СОБАКА по имени " + dogname);
         int d = cs.nextInt();
         name1.Run(a);
         name2.Run(b);
@@ -105,6 +144,34 @@ class Main {
         System.out.println("Всего котов понадобилось " + Cat.getCount());
         System.out.println("Всего собак понадобилось " + Dog.getCount());
 
+        feedingCats();
+    }
+        public static void feedingCats(String[] args){
+        int action;
+        Catfeed[] cats = new Catfeed[5];
+        cats[0] = new Catfeed("Василий", 21, false);
+        cats[0] = new Catfeed("Тимоха", 7, false);
+        cats[0] = new Catfeed("Кукуруза", 15, false);
+        cats[0] = new Catfeed("Хвостокрыл", 39, false);
+        cats[0] = new Catfeed("Усач", 28, false);
+
+        Bowl bowl = new Bowl(40);
+        bowl.total();
+        for(int i = 0; i < cats.length; i++){
+            if (cats[i].full == false && cats[i].hungry < bowl.feed ){
+                cats[i].feedAdd(bowl);
+                cats[i].full = true;
+                System.out.println("Пушистый звереныш по имени " + cats[i].name + " хорошо покушал и он сыт");
+            } else {
+                System.out.println("Пушистый звереныш по имени " + cats[i].name + " очень голоден и хочет кушать");
+
+            }
+        }
+        bowl.total();
+        System.out.println("Добавьте пожалуйста корма в миску для того чтобы котик покушал - укажите вес корма для добавления");
+        action = sc.nextInt();
+        bowl.addfeed(action);
+        bowl.total();
     }
 }
 
