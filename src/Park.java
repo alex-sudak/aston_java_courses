7fce993444a028598d78687a4e7e314bd313062c
alex-sudak <al13khattab@gmail.com>
2024-06-04 18:42:55 +0300

    changes for array - put it inside Park class

--- src/Park.java
+++ src/Park.java
@@ -1,5 +1,4 @@
 public class Park {
-
     public static class Attractions {
         private String name;
         private String time;
@@ -14,7 +13,25 @@ public class Park {
             this.price = price;
         }
 
-        public int getNumber() { return number; }
+        public Attractions[] attractions () {
+            attractions[0] = new Park.Attractions(1, "Колесо обозрения", "8:00 - 19:00", 3);
+            attractions[1] = new Park.Attractions(2, "Лодочка", "9:00 - 19:00", 5);
+            attractions[2] = new Park.Attractions(3, "Супер8", "8:00 - 18:30", 7);
+            attractions[3] = new Park.Attractions(4, "Вальс", "8:30 - 17:30", 4);
+            return attractions();
+        }
+
+        public void setAttractions(Attractions[] attractions){
+            this.attractions = attractions;
+        }
+
+        public Attractions[] getAttractions(){
+            return attractions();
+        }
+
+        public int getNumber() {
+            return number;
+        }
 
         public String getName() {
             return name;
@@ -28,7 +45,9 @@ public class Park {
             return price;
         }
 
-        public void setNumber(int number) { this.number = number; }
+        public void setNumber(int number) {
+            this.number = number;
+        }
 
         public void setTime(String time) {
             this.time = time;
@@ -41,5 +60,13 @@ public class Park {
         public void setName(String name) {
             this.name = name;
         }
+
+        public void parkInfo() {
+            System.out.println("Информация об аттракционе номер : " + number);
+            System.out.println("Название: " + name);
+            System.out.println("Стоимость(руб): " + price);
+            System.out.println("Время работы: " + time);
+            System.out.println();
+        }
     }
-}
+    }
