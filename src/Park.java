bedaf92071de7dc7f3d63ef784c01026805fc75e
alex-sudak <al13khattab@gmail.com>
2024-06-05 13:56:00 +0300

    changes for array and get/set - transferred for Park class all

--- src/Park.java
+++ src/Park.java
@@ -1,10 +1,64 @@
 public class Park {
-    public static class Attractions {
+    private String name;
+    private String time;
+    private int price;
+    private int number;
+    private Attractions[] attractions;
+
+    Park park = new Park();
+    Park.Attractions[] attractions1 = new Park.Attractions[4]; {
+        attractions[0] = new Park.Attractions(1, "Колесо обозрения", "8:00 - 19:00", 3);
+        attractions[1] = new Park.Attractions(2, "Лодочка", "9:00 - 19:00", 5);
+        attractions[2] = new Park.Attractions(3, "Супер8", "8:00 - 18:30", 7);
+        attractions[3] = new Park.Attractions(4, "Вальс", "8:30 - 17:30", 4);
+        park.setAttractions(attractions);
+    }
+
+    public void setAttractions(Attractions[] attractions) {
+        this.attractions = attractions;
+    }
+
+    public Attractions[] getAttractions() {
+        return attractions1;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public class Attractions {
         private String name;
         private String time;
         private int price;
         private int number;
-        private Attractions[] attractions;
 
         public Attractions(int number, String name, String time, int price) {
             this.number = number;
@@ -13,60 +67,13 @@ public class Park {
             this.price = price;
         }
 
-        public Attractions[] attractions () {
-            attractions[0] = new Park.Attractions(1, "Колесо обозрения", "8:00 - 19:00", 3);
-            attractions[1] = new Park.Attractions(2, "Лодочка", "9:00 - 19:00", 5);
-            attractions[2] = new Park.Attractions(3, "Супер8", "8:00 - 18:30", 7);
-            attractions[3] = new Park.Attractions(4, "Вальс", "8:30 - 17:30", 4);
-            return attractions();
-        }
-
-        public void setAttractions(Attractions[] attractions){
-            this.attractions = attractions;
-        }
-
-        public Attractions[] getAttractions(){
-            return attractions();
-        }
-
-        public int getNumber() {
-            return number;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getTime() {
-            return time;
-        }
-
-        public int getPrice() {
-            return price;
-        }
-
-        public void setNumber(int number) {
-            this.number = number;
-        }
-
-        public void setTime(String time) {
-            this.time = time;
-        }
-
-        public void setPrice(int price) {
-            this.price = price;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void parkInfo() {
+        public void attractionInfo() {
             System.out.println("Информация об аттракционе номер : " + number);
             System.out.println("Название: " + name);
             System.out.println("Стоимость(руб): " + price);
             System.out.println("Время работы: " + time);
             System.out.println();
+//            return getAttractions();
         }
     }
-    }
+}
--- src/ParkInfo.java
+++ src/ParkInfo.java
@@ -2,17 +2,13 @@ import java.util.Arrays;
 
 class ParkInfo {
     public static void main(String[] args) {
-//        Attractions[] attractions = new Park.Attractions[4];
-//        attractions[0] = new Park.Attractions(1, "Колесо обозрения", "8:00 - 19:00", 3);
-//        attractions[1] = new Park.Attractions(2, "Лодочка", "9:00 - 19:00", 5);
-//        attractions[2] = new Park.Attractions(3, "Супер8", "8:00 - 18:30", 7);
-//        attractions[3] = new Park.Attractions(4, "Вальс", "8:30 - 17:30", 4);
 
-        Park.Attractions[] attr = new Park.Attractions[4];
+        Park.Attractions[] attractions = new Park.Attractions[4];
 
-        for (int i = 0; i < attr.length; i++) {
-            System.out.println(attr[i].getAttractions());
-//            System.out.println("Информация об аттракционе номер : " + attr[i].getNumber());
+        for (int i = 0; i < attractions.length; i++) {
+            attractions[i].attractionInfo();
+//            System.out.println(attractions[i].attractionInfo());
+//            System.out.println("Информация об аттракционе номер : " + attractions[i].attractioninfo()
 //            System.out.println("Название: " + attr[i].getName());
 //            System.out.println("Стоимость(руб): " + attr[i].getPrice());
 //            System.out.println("Время работы: " + attr[i].getTime());
