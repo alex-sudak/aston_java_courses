f37e0e0c53c1f3faf0e29855ae88ab108d587ea1
alex-sudak <al13khattab@gmail.com>
2024-06-06 15:55:07 +0300

    small changes between lines and methods

--- src/Park.java
+++ src/Park.java
@@ -27,13 +27,6 @@ public class Park {
         }
     }
 
-//    private Park park = new Park();
-//    Park.Attraction park1= new Park.Attraction("Колесо обозрения","8:00 - 19:00",3);
-//    Park.Attraction park2 = new Park.Attraction( "Лодочка", "9:00 - 19:00", 5);
-//    Park.Attraction park3 = new Park.Attraction( "Супер8", "8:00 - 18:30", 7);
-//    Park.Attraction park4 = new Park.Attraction( "Вальс", "8:30 - 17:30", 4);
-
-
     public class Attraction {
         private String name;
         private String time;
--- src/ParkInfo.java
+++ src/ParkInfo.java
@@ -4,12 +4,16 @@ class ParkInfo {
     public static void main(String[] args) {
 
         Park park = new Park(4);
+
         Park.Attraction attraction1 = park.new Attraction( "Колесо обозрения", "8:00 - 19:00", 3);
         park.addAttraction(attraction1);
+
         Park.Attraction attraction2 = park.new Attraction( "Лодочка", "9:00 - 19:00", 5);
         park.addAttraction(attraction2);
+
         Park.Attraction attraction3 = park.new Attraction( "Супер8", "8:00 - 18:30", 7);
         park.addAttraction(attraction3);
+
         Park.Attraction attraction4 = park.new Attraction( "Вальс", "8:30 - 17:30", 4);
         park.addAttraction(attraction4);
 
